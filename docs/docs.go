d964243bf7369e8c5955a2482dd0116d9147d9eb
Mladen Dryankov <mdryankov@gmail.com>
2021-08-31 15:00:38 +0300

    Fix doc version #13

--- docs/docs.go
+++ docs/docs.go
@@ -871,7 +871,7 @@ type swaggerInfo struct {
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = swaggerInfo{
-	Version:     "0.1.4.2",
+	Version:     "0.1.5",
 	Host:        "localhost",
 	BasePath:    "/notifications/api",
 	Schemes:     []string{"https"},
