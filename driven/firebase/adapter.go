522c68c1a57312f772c16efae22fd9a1b1060d87
Stephen Hurwit <sjhurwit@gmail.com>
2022-06-17 11:54:52 -0500

    add license headers

--- driven/firebase/adapter.go
+++ driven/firebase/adapter.go
@@ -1,13 +1,28 @@
+// Copyright 2022 Board of Trustees of the University of Illinois.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package firebase
 
 import (
 	"context"
-	"firebase.google.com/go"
-	"firebase.google.com/go/messaging"
 	"fmt"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
-	"log"
 )
 
 // Adapter entity
